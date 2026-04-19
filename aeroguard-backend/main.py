f328d600af9c660f068a04a8c74e603ec2f50e3c
Naps03 <napoleonnzeko35@gmail.com>
2026-04-19 18:07:46 +0200

    Strukturierung der SQLite-Datenbank

--- aeroguard-backend/main.py
+++ aeroguard-backend/main.py
@@ -1,13 +1,13 @@
-
+import sqlite3
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# Autoriser le Frontend (React) à communiquer avec le Backend
+# Erstellung der Kommunikation zwischen FrontEnd & BackEnd
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # On pourra restreindre plus tard
+    allow_origins=["*"], 
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -18,10 +18,40 @@ def read_root():
 
 @app.get("/api/sensor-test")
 def get_sensor_test():
-    # Simulation de données pour tester la connexion
+    # Simualtion der Sensordaten zum Test
     return {
         "co2": 1100,
         "temperature": 30,
         "humidity": 48,
         "iaq_score": 90
     }
+
+# Initialiserung der Datenbank
+def init_db():
+    conn = sqlite3.connect("aeroguard.db")
+    cursor = conn.cursor()
+    # Sensordaten-Tabelle
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS measurements (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+            co2 INTEGER,
+            temperature REAL,
+            humidity REAL,
+            iaq_score INTEGER
+        )
+    ''')
+    # Belegungsplan-Tabelle
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS occupations (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            day TEXT,
+            start_time TEXT,
+            end_time TEXT,
+            label TEXT
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+init_db()
