6953964e5e6d9840de0b5afa953f86164caa6efa
Naps03 <napoleonnzeko35@gmail.com>
2026-06-03 19:00:10 +0200

    ESP32 Dev Code mit Auslesen der Raumluftdaten & MQTT Einsatz

--- esp32_Dev_Module/esp32_Dev_Module.ino
+++ esp32_Dev_Module/esp32_Dev_Module.ino
@@ -1,40 +1,50 @@
+#include <Wire.h>
+#include <SensirionI2CScd4x.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
-#include <ArduinoJson.h>
-#include <SensirionI2CScd4x.h>
-#include <Wire.h>
 
-//MQTT-Parameter
-const char* ssid = "WIFI_NAME";
-const char* password = "WIFI_PASSWORT";
-const char* mqtt_server = "COMPUTER_IP"; 
-const int mqtt_port = 1883;
-const char* mqtt_topic = "aeroguard/room1";
-
-const int LDC2410_PIN = 13;
+const char* ssid = "WLAN-Name";   
+const char* password = "WLAN-Passwort"; 
+const char* mqtt_server = "100.81.219.149";   
+const int mqtt_port = 1883;   
 
 SensirionI2CScd4x scd41;
 WiFiClient espClient;
 PubSubClient client(espClient);
+
 unsigned long lastMsg = 0;
 
 void setup_wifi() {
   delay(10);
+  Serial.println();
+  Serial.print("Verbindung zur WLAN : ");
+  Serial.println(ssid);
+
   WiFi.begin(ssid, password);
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
+    Serial.print(".");
   }
-  Serial.println("\nWi-Fi verbunden !");
+
+  Serial.println("");
+  Serial.println("WLAN verbunden!");
+  Serial.print("ESP32 IP-Adresse : ");
+  Serial.println(WiFi.localIP());
 }
 
+// Verbindung zum Broker
 void reconnect() {
   while (!client.connected()) {
-    String clientId = "ESP32Client-AeroGuard-" + String(random(0, 1000));
+    Serial.print("Verbindung zum Mosquitto...");
+    String clientId = "ESP32Client-AeroGuard";
+    
     if (client.connect(clientId.c_str())) {
-      Serial.println("verbunden !");
+      Serial.println("Verbunden mit dem Broker !");
     } else {
-      Serial.print("Verbindung fehlgeschlagen, rc=");
+      Serial.print("Verbindung mit dem Broker fehlgeschlagen, ErrorCode = ");
       Serial.print(client.state());
+      Serial.println("5 Sekunden warten...");
       delay(5000);
     }
   }
@@ -42,29 +52,21 @@ void reconnect() {
 
 void setup() {
   Serial.begin(115200);
+  while (!Serial) { delay(100); }
   
-  setup_wifi();
   client.setServer(mqtt_server, mqtt_port);
 
-  pinMode(LDC2410_PIN, INPUT);
-
-  Wire.begin();
+  Wire.begin(21, 22); // SDA = 21, SCL = 22 auf ESP32
   scd41.begin(Wire);
-
-  uint16_t error;
-  char errorMessage[256];
   
-  // Stoppen des SCD41, Falls eine Messung schon aktiv war
-  error = scd41.stopPeriodicMeasurement();
+  scd41.stopPeriodicMeasurement();
+  uint16_t error = scd41.startPeriodicMeasurement();
   
-  // Starten der periodeshen Messung von CO2/Temp/Hum
-  error = scd41.startPeriodicMeasurement();
   if (error) {
-    Serial.print("Erreur lors du démarrage du SCD41 : ");
-    errorToString(error, errorMessage, 256);
-    Serial.println(errorMessage);
+    Serial.print("Fehler bei Initialisierung des SCD41. Code : ");
+    Serial.println(error);
   } else {
-    Serial.println("Capteur SCD41 prêt et démarré !");
+    Serial.println("SCD41 bereit");
   }
 }
 
@@ -75,31 +77,32 @@ void loop() {
   client.loop();
 
   unsigned long now = millis();
-  
+
   if (now - lastMsg > 5000) {
     lastMsg = now;
-
-    //Menschliche Präsenz (LDC2410)
-    bool presence = (digitalRead(LDC2410_PIN) == HIGH);
-
-    //Raumluftdaten (SCD41)
     uint16_t co2 = 0;
-    float temperatur = 0.0;
-    float feuchtigkeit = 0.0;
-    bool dataReady = false;
+    float temp = 0.0, hum = 0.0;
+    bool ready = false;
+    
+    uint16_t error = scd41.getDataReadyFlag(ready);
+    
+    if (!error && ready) {
+      error = scd41.readMeasurement(co2, temp, hum);
+      
+      if (!error) {
+        Serial.print("CO2: "); Serial.print(co2); Serial.print(" ppm\t");
+        Serial.print("Temp: "); Serial.print(temp, 1); Serial.print(" °C\t");
+        Serial.print("Hum: "); Serial.print(hum, 1); Serial.println(" %");
 
-    // Prüfen ob SCD41 neue Werte hat
-    scd41.getDataReadyFlag(dataReady);
+        String co2Str = String(co2);
+        String tempStr = String(temperature, 1);
+        String humStr = String(humidity, 1);
 
-    //Erstellen der JSON & Senden
-    StaticJsonDocument<128> doc;
-    doc["co2"] = co2;
-    doc["temperature"] = temperature;
-    doc["humidity"] = humidity;
-    doc["presence"] = presence;
-
-    char buffer[128];
-    serializeJson(doc, buffer);
-    client.publish(mqtt_topic, buffer);
-  }
+        client.publish("aeroguard/co2", co2Str.c_str());
+        client.publish("aeroguard/temp", tempStr.c_str());
+        client.publish("aeroguard/hum", humStr.c_str());
+      }
+    }
+  }  
+    delay(100); 
 }
