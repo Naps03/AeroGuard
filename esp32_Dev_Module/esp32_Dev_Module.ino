dce558188b0734dd0c5913e1fe5986bb28026873
Naps03 <napoleonnzeko35@gmail.com>
2026-06-12 02:01:13 +0200

    Code zur Steuerung von LED & LD2410C hinzugefügt

--- esp32_Dev_Module/esp32_Dev_Module.ino
+++ esp32_Dev_Module/esp32_Dev_Module.ino
@@ -1,18 +1,28 @@
+
 #include <Wire.h>
 #include <SensirionI2CScd4x.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <ld2410.h>
 
 const char* ssid = "Vodafone-1B54";   
 const char* password = "EY36k2chKFRsnyry"; 
 const char* mqtt_server = "192.168.0.94";   
-const int mqtt_port = 1883;   
+const int mqtt_port = 1883;  
+
+const int ledVerte = 13;
+const int ledJaune = 12;
+const int ledRouge = 14;
 
 SensirionI2CScd4x scd41;
-WiFiClient espClient;
+WiFiClient espClient; 
 PubSubClient client(espClient);
+ld2410 radar;
 
 unsigned long lastMsg = 0;
+unsigned long lastPresenceTime = 0;
+bool systemActive = false;            
+const unsigned long TIMEOUT_VEILLE = 60000; //Wartezeit bis das System augeht, wenn niemand mehr im Raum ist
 
 void setup_wifi() {
   delay(10);
@@ -52,13 +62,24 @@ void reconnect() {
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial) { delay(100); }
+
+  pinMode(ledVerte, OUTPUT);
+  pinMode(ledJaune, OUTPUT);
+  pinMode(ledRouge, OUTPUT);
   
+  Serial2.begin(256000, SERIAL_8N1, 32, 27); 
+    Serial.print("Initialisierung des LD2410C... ");
+    if (radar.begin(Serial2)) {
+      Serial.println("LD2410 Bereit !");
+    } else {
+      Serial.println("Fehler bei Initialisierung des SCD41");
+    }
+
   setup_wifi();
   
   client.setServer(mqtt_server, mqtt_port);
 
-  Wire.begin(21, 22); // SDA = 21, SCL = 22 auf ESP32
+  Wire.begin(25, 26); // SDA = 21, SCL = 22 auf ESP32
   scd41.begin(Wire);
   
   scd41.stopPeriodicMeasurement();
@@ -78,33 +99,89 @@ void loop() {
   }
   client.loop();
 
+  radar.read();
+  bool menschPraesenz = radar.movingTargetDetected() || radar.stationaryTargetDetected();
+
   unsigned long now = millis();
-
-  if (now - lastMsg > 5000) {
-    lastMsg = now;
-    uint16_t co2 = 0;
-    float temp = 0.0, hum = 0.0;
-    bool ready = false;
+  
+  if (menschPraesenz) {
+    lastPresenceTime = now;
     
-    uint16_t error = scd41.getDataReadyFlag(ready);
-    
-    if (!error && ready) {
-      error = scd41.readMeasurement(co2, temp, hum);
-      
-      if (!error) {
-        Serial.print("CO2: "); Serial.print(co2); Serial.print(" ppm\t");
-        Serial.print("Temp: "); Serial.print(temp, 1); Serial.print(" °C\t");
-        Serial.print("Hum: "); Serial.print(hum, 1); Serial.println(" %");
-
-        String co2Str = String(co2);
-        String tempStr = String(temp, 1);
-        String humStr = String(hum, 1);
-
-        client.publish("aeroguard/co2", co2Str.c_str());
-        client.publish("aeroguard/temp", tempStr.c_str());
-        client.publish("aeroguard/hum", humStr.c_str());
-      }
+    if (!systemActive) {
+      systemActive = true;
+      Serial.println("Menschliche Praesenz detektiert. Neu Starten des Aeroguard");
+      client.publish("aeroguard/presence", "1");
     }
-  }  
-    delay(100); 
+  } else {
+    if (systemActive && (now - lastPresenceTime > TIMEOUT_VEILLE)) {
+      systemActive = false;
+      Serial.println("Leerer Raum seit 60s. Aktivierung des Sparmodus");
+      eteindreToutesLesLEDs();
+      client.publish("aeroguard/presence", "0");
+    }
+  }
+  
+  if (systemActive) {
+    if (now - lastMsg > 5000) {
+      lastMsg = now;
+      uint16_t co2 = 0;
+      float temp = 0.0, hum = 0.0;
+      bool ready = false;
+      
+      uint16_t error = scd41.getDataReadyFlag(ready);
+      
+      if (!error && ready) {
+        error = scd41.readMeasurement(co2, temp, hum);
+        
+        if (!error) {
+          Serial.print("CO2: "); Serial.print(co2); Serial.print(" ppm\t");
+          Serial.print("Temp: "); Serial.print(temp, 1); Serial.print(" °C\t");
+          Serial.print("Hum: "); Serial.print(hum, 1); Serial.println(" %");
+
+          gererLEDs(co2);
+
+          String co2Str = String(co2);
+          String tempStr = String(temp, 1);
+          String humStr = String(hum, 1);
+
+          client.publish("aeroguard/co2", co2Str.c_str());
+          client.publish("aeroguard/temp", tempStr.c_str());
+          client.publish("aeroguard/hum", humStr.c_str());
+        }
+      }
+    } 
+  } else {
+    if (now - lastMsg > 15000) {
+      lastMsg = now;
+      Serial.println("💤 Système en sommeil... En attente d'une présence.");
+    }
+  }
+    delay(500); 
+}
+
+void gererLEDs(uint16_t valeurCO2) {
+  if (valeurCO2 < 800) {
+    // Air Sain -> Vert uniquement
+    digitalWrite(ledVerte, HIGH);
+    digitalWrite(ledJaune, LOW);
+    digitalWrite(ledRouge, LOW);
+  } 
+  else if (valeurCO2 >= 800 && valeurCO2 <= 1200) {
+    // Attention -> Jaune uniquement
+    digitalWrite(ledVerte, LOW);
+    digitalWrite(ledJaune, HIGH);
+    digitalWrite(ledRouge, LOW);
+  } 
+  else {
+    // Alerte Ventilation -> Rouge uniquement
+    digitalWrite(ledVerte, LOW);
+    digitalWrite(ledJaune, LOW);
+    digitalWrite(ledRouge, HIGH);
+  }
+}
+
+void eteindreToutesLesLEDs() {
+  digitalWrite(ledVerte, LOW);
+  digitalWrite(ledJaune, LOW);
+  digitalWrite(ledRouge, LOW);
 }
